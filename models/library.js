b37a55136140a4ee459e218ebd2e8aeaac894621
Vadn7 <96883715+Vadn7@users.noreply.github.com>
2024-07-23 17:00:04 +0100

    Adding test

--- models/library.js
+++ models/library.js
@@ -1,66 +1,3 @@
-/*
-
-class Book {
-    constructor(title, author, isbn, copies) {
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
-      this.copies = copies;
-    }
-  
-    getAvailability() {
-      return this.copies > 0 ? "Available" : "Out of stock";
-    }
-  
-
-    getTitle(){ return this.title;     }
- 
-    setTitle(value){ this._title=value;     }
-
-
-    getAuthor(){ return this._author;     }
-
-    setAuthor(value){ this._author=value  ;     }
-
-
-    getIsbn(){  return this._isbn;     }
-
-    setIsbn(value){  this._isbn=value;     }
-
-
-    getCopies() { return  this._copies;  }
-
-    setCopies(value) {  this._copies=value;  }
-
-
-    getAvailable() {  return this._copies > 0 ? "Available" : "Out of stock" }
-
-}
-
-
-  class EBook extends Book {
-    constructor(title, author, isbn, copies, fileSize) {
-      super(title, author, isbn, copies);
-      this.fileSize = fileSize; // Taille du fichier en MB
-    }
-  
-    getFormat() {
-      return `EBook (${this.fileSize} MB)`;
-    }
-  }
-  
-  class AudioBook extends Book {
-    constructor(title, author, isbn, copies, duration) {
-      super(title, author, isbn, copies);
-      this.duration = duration; // Durée en heures
-    }
-  
-    getFormat() {
-      return `AudioBook (${this.duration} hours)`;
-    }
-  }
-*/
-
 class Library {
     constructor() {
       this.books = [];
@@ -107,21 +44,5 @@ class Library {
 
 
 }
-/*  
-  let library = new Library();
-  library.addBook(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084", 3));
-  library.addBook(new EBook("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565", 2, 1.5));
-  library.addBook(new AudioBook("Moby Dick", "Herman Melville", "9781503280786", 4, 24));
-  
-  console.log(library.listBooks());
-
-  library.removeBook("9780061120084");
-  console.log(library.listBooks());
-  
-  console.log(library.findBook("9780743273565"));
-
-  library.borrowBooks("9780743273565");
-
-  console.log(library.findBook("9780743273565"));*/
 
   module.exports=Library;
